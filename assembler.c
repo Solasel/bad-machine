5d3da5ea99f8b8a1969e5f1c8b2b7586ba1f0fdb
William Walker <wewalker@berkeley.edu>
2017-10-13 23:08:35 -0700

    Progress on assembler.c.

--- assembler.c
+++ assembler.c
@@ -1,32 +1,69 @@
 /*
    Takes a file with bal commdands and turns it into
-   a file with equivalent bml instructions.
+   a file with equivalent bml instructions to be
+   passed into logisim and be run.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-/* Parses bal code in source into bml code in dest. */
-parse(char *src, char *dst);
+#define NUM_INSTRUCTIONS (27)
 
-/* Writes the bal commands in src_lines into dst as
- * bml instructions readable by logisim. */
+const char *instructions[] = {
+	"add", "sub",
+	"slt", "sltu",
+	"sll",
+	"sra", "srl",
+	"or", "nor",
+	"and", "nand",
+	"xor", "xnor",
+	"mul", "mulh", "div", "rem",
+
+	"addi", "slti", "slli", "srai",
+	"ori", "andi", "xori", "lw",
+
+	"sw",
+
+	"lui"
+};
+
+const char *registers[] = {
+	"x0", "x1", "x2", "x3",
+	"x4", "x5", "x6", "x7"
+};
+
+/* Parses bal code in source into bml instructions in dest. */
 /* TODO */
-write_bml(char *dst, char **src_lines);
+int assemble_and_write(const char *src, const char *dst);
 
-/* Reads the contents of src and puts it into buf. */
+/* Gets a null-terminated list of the instructions
+ * in src_text and writes it to inst_lst. */
 /* TODO */
-read_file(char *src, char **buf);
+int get_inst_lst(char *src_text, char ***inst_lst);
 
-/* Reads the lines of src and puts a null-terminated 
- * list of its lines into lines. */
+/* Converts line into a hex instruction
+ * and stores that in dst. */
 /* TODO */
-separate_lines(char *src, char ***lines);
+int line_to_inst(int i, char **line, char **dst);
+/* Writes inst_lst to dst in a format that logisim gets. */
+/* TODO */
+int write_instuctions(**inst_lst, const char *dst);
+
+/* Reads the contents of src and puts it into buf.
+ * buf is double-null terminated. */
+/* TODO */
+int read_file(const char *src, char **buf);
 
 /* Frees a null-terminated list of strings. */
 /* TODO */
-free_str_lst(char **str_lst);
+int free_str_lst(char **str_lst);
+
+/* Writes the low 5 bits of the given string
+ * into the given I-type instruction. */
+/* TODO */
+int write_i_type_imm(char *imm, int *imm);
 
 /* Main. */
 int main(int argc, char *argv[])
@@ -46,61 +83,144 @@ int main(int argc, char *argv[])
 	printf("Attempting to parse %s into machine language -> %s...",
 			argv[1], argv[2]); 
 
-	failure = parse(argv[1], argv[2]);
+	failure = assemble_and_write(argv[1], argv[2]);
 
 end:
 
 	/* Handles errors and tells the user what happened. */
 	if (failure)
-		printf("Parsing failed, neither file has been changed.\n");
-		printf("Error code: %d\n", failure);
+		printf("Parsing failed, neither file has been changed.\n"
+		"Error code: %d\n", failure);
 
 	else 
-		printf("Parsing successful!\n");
-		printf("%s now contains bad machine langauge!\n", argv[2]);
+		printf("Parsing successful!\n"
+		"%s now contains bad machine langauge!\n", argv[2]);
 
 	return failure;
 }
 
-/* Parses bal code in source into bml code in dest. */
-int parse(char *src, char *dst)
+/* Parses bal code in source into bml instructions in dest. */
+int assemble_and_write(const char *src, const char *dst)
 {
 	int failure;
 
 	char *src_buf;
-	char **src_lines;
+	char **inst_lst;
 
-	/* Reads the contents of src into src_buf. */
-	failure = read_file(source, &src_buf);
+	/* Reads the contents of src into src_buf (double null terminated). */
+	failure = read_file(src, &src_buf);
 	if (failure) {
 		printf("Failed to read %s at %d.\n",
-				src, __LINE__ - 2);
+				src, __LINE__);
 
 		return failure;
 	}
 
-	/* Separates src_buf into lines (commands) and puts
-	 * them into a null-terminated list src_lines. */
-	failure = separate_lines(src_buf, &src_lines);
+	/* Loads the hex instructions into instructions. */
+	failure = get_inst_lst(src_buf, &inst_lst);
 	if (failure) {
-		printf("Failed to separate %s into lines at %d.\n",
-				src, __LINE__ - 2);
+		printf("Failed to get a list of instructions at %d.\n",
+				__LINE__);
+		goto free_src_buf;
+	}
 
-		goto free_src_lines;
+	/* Writes the list of instructions to dst. */
+	failure = write_instuctions(inst_lst, dst);
+	if (failure)
+		printf("Failed to write instructions to %s at %d.\n",
+				dst, __LINE__);
+
+
+	/* Free things. */
+	free_str_lst(inst_lst);
+free_src_buf:
+	free(src_buf);
+	return failure;
+}
+
+
+/* Gets a null-terminated list of the instructions
+ * in src_text and writes it to buf. */
+int get_inst_lst(char *src_text, char ***inst_lst)
+{
+	int failure;
+	int i;
+	char *line;
+
+	/* Gets the number of instructions. */
+	int inst_count = 0;
+
+	i = 0;
+	while (src_text[i]) {
+		if (src_text[i] == '\n')
+			inst_count++;
+		i++;
+	}
+
+	/* Initializes a null-terminated array
+	 * to write instructions to. */
+	*inst_lst = malloc((inst_count + 1) * sizeof(char *));
+	if (!inst_lst) {
+		printf("Failed to malloc a list of instructions at %d.\n",
+				__LINE__);
+		return 1;
+	}
+
+	*inst_lst[inst_count] = NULL;
+
+	/* For each line in src_text, writes
+	 * its instruction to inst_lst. */
+	i = 0;
+	while ((line = strtok(src_text, "\r\n"))) {
+
+		failure = line_to_inst(i, &line, *inst_lst + i);
+		if (failure) {
+			printf("Failed to change a line to an instruction at "
+					"%d.\n", __LINE__);
+			goto free_inst_lst;
+		}
+
+		src_text = strchr(line, '\0') + 1;
+
+		i++;
+	}
+
+free_inst_lst:
+	free_str_lst(inst_lst);
+	return failure
+
+}
+
+/* Writes the bml equivalent of the bal in linep
+ * into dst, in hex. */
+int line_to_inst(int i, char **linep, char **dst)
+{
+	uint16_t i;
+
+	int instr = 0;
+	char *line = *linep;
+	char *token;
+
+	/* Writes OPC, S1, and S2. */
+	token = strtok(line, " \t\v\f");
+	for (i = 0; i < NUM_INSTRUCTIONS; i++) {
+
+		if (!strcmp(line, instructions[i])) {
+			
+			switch(i) {
+
+			}
+
+			break;
+
+		}
 	}
 	
-	/* Writes the commands (in src_lines) into
-	 * dst, finishing the process. */
-	failure = write_bml(dst, src_lines);
-	if (failure)
-		printf("Failed to write bml to %s at %d.\n",
-				dst, __LINE__ - 2);
-	
-	/* Frees things. */
-	free(src_buf);
-free_src_lines:
-	free_str_lst(src_lines);
+	if (i == NUM_INSTRUCTIONS) {
+		printf("Invalid instruction: %s at line %d.\n", line, i);
+		return 1;
+	}
 
-	return failure;
+
 }
 

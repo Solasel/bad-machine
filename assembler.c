da13ff0b44a56737cfd3b34b7e2dae5a78b21cb9
William Walker <wewalker@berkeley.edu>
2017-10-14 20:38:47 -0700

    Finishes first assembler.c rough draft. 100% buggy
    Also adds a views file for assembler.c and clarifies
    some things in notes.txt.

--- assembler.c
+++ assembler.c
@@ -4,12 +4,14 @@
    passed into logisim and be run.
 */
 
+/* Constants. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 
 #define NUM_INSTRUCTIONS (27)
+#define NUM_REGISTER (8)
 
 const char *instructions[] = {
 	"add", "sub",
@@ -34,6 +36,8 @@ const char *registers[] = {
 	"x4", "x5", "x6", "x7"
 };
 
+/* Function Prototypes. */
+
 /* Parses bal code in source into bml instructions in dest. */
 int assemble_and_write(const char *src, const char *dst);
 
@@ -41,60 +45,80 @@ int assemble_and_write(const char *src, const char *dst);
  * in src_text and writes it to inst_lst. */
 int get_inst_lst(char *src_text, char ***inst_lst);
 
-/* Converts line into a hex instruction
- * and stores that in dst. */
-/* TODO */
-int line_to_inst(int i, char **line, char **dst);
+/* Writes the bml equivalent of the bal in linep
+ * into dst, in hex. */
+int line_to_inst(int i, char **linep, char **dst);
 
 /* Writes inst_lst to dst in a format that logisim gets. */
-/* TODO */
 int write_instuctions(**inst_lst, const char *dst);
 
 /* Reads the contents of src and puts it into buf.
  * buf is double-null terminated. */
-/* TODO */
 int read_file(const char *src, char **buf);
 
 /* Frees a null-terminated list of strings. */
-/* TODO */
 int free_str_lst(char **str_lst);
 
+/* Formats instr as an R-style instruction
+ * with given arg1, arg2, and arg3, and
+ * S1 and S2. */
+int write_r_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3,
+		int s1, int s2);
+
+/* Formats instr as an I-style instruction
+ * with given arg1, arg2, and arg3, and S1. */
+int write_i_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3,
+		int s1);
+
+/* Formats instr as an S-style instruction
+ * with given arg1, arg2, and arg3. */
+int write_s_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3);
+
+/* Formats instr as an U-style instruction
+ * with given arg1 and arg2. */
+int write_u_type(int line, uint16_t *instr,
+		char *arg1, char *arg2);
+
 /* Sets the OPC field of instr to the given value. */
-/* TODO */
 int set_opc(int opc, uint16_t *instr);
 
-/* Sets the S0 field of instr to the given value. */
-/* TODO */
-int set_s0(int s0, uint16_t *instr);
-
 /* Sets the S1 field of instr to the given value. */
-/* TODO */
 int set_s1(int s1, uint16_t *instr);
 
-/* Determines if arg is a valid register, and if so,
- * writes it to the rsX field of instr. */
-/* TODO */
-int decode_rsX(char *arg, uint16_t *instr);
+/* Sets the S2 field of instr to the given value. */
+int set_s2(int s2, uint16_t *instr);
+
+/* Makes sure that reg is a valid register. */
+int is_invalid_reg(char *reg)
+
+/* Given a valid register arg,
+ * writes it to the rsX field of instr.
+ * x < 0 ==> rs2
+ * x = 0 ==> rs0
+ * x > 0 ==> rs1	*/
+int decode_rsx(int x, char *arg, uint16_t *instr);
+
+/* Makes sure that imm is a valid decimal number. */
+int is_invalid_imm(char *imm);
 
 /* Parses imm as a signed value, and writes
  * it to I-type instr appropriately. */
-/* TODO */
 int decode_i_type_imm(char *imm, uint16_t *instr);
 
 /* Parses imm as a signed value, and writes
  * it to S-Type instr appropriately. */
-/* TODO */
 int decode_s_type_imm(char *imm, uint16_t *instr);
 
 /* Parses imm as a signed value, and writes
  * it to U-Type instr appropriately. */
-/* TODO */
 int decode_u_type_imm(char *imm, uint16_t *instr);
 
 /* Takes instr and converts it into a 4-digit
- * hex representation for writing. */
-/* TODO */
-char *instr_to_hex(uint16_t instr);
+ * hex representation, stored in buf. */
+int instr_to_hex(uint16_t instr, char **buf);
 
 /* Gets the nth bit of x. */
 unsigned get_bit(unsigned x, unsigned n);
@@ -135,6 +159,14 @@ end:
 
 	return failure;
 }
+
+
+/*
+ * #################
+ * ### MAIN CODE ###
+ * #################
+*/
+
 
 /* Parses bal code in source into bml instructions in dest. */
 int assemble_and_write(const char *src, const char *dst)
@@ -185,13 +217,25 @@ int get_inst_lst(char *src_text, char ***inst_lst)
 	char *line;
 
 	/* Gets the number of instructions. */
-	int inst_count = 0;
+	int inst_count = 1;
+	char *temp_src = strdup(src_text);
 
-	i = 0;
-	while (src_text[i]) {
-		if (src_text[i] == '\n')
-			inst_count++;
-		i++;
+	if (!strtok(temp_src, "\r\n")) {
+		printf("Empty source file.\n")
+		failure = -1;
+		goto free_temp_src;
+	}
+
+	while (strtok(NULL, "\r\n"))
+		inst_count++;
+
+	printf("%d instructions in source file.\n", inst_count);
+
+	if (inst_count > 256) {
+		printf("Too many instructions in source file.\n"
+				"You can put a max of 256 instructions.\n");
+		failure = -1;
+		goto free_temp_src;
 	}
 
 	/* Initializes a null-terminated array
@@ -200,7 +244,8 @@ int get_inst_lst(char *src_text, char ***inst_lst)
 	if (!inst_lst) {
 		printf("Failed to malloc a list of instructions at %d.\n",
 				__LINE__);
-		return 1;
+		failure = 1;
+		goto free_temp_src;
 	}
 
 	*inst_lst[inst_count] = NULL;
@@ -224,29 +269,216 @@ int get_inst_lst(char *src_text, char ***inst_lst)
 
 free_inst_lst:
 	free_str_lst(inst_lst);
+free_temp_src:
+	free(temp_src);
 	return failure
 
 }
 
+
 /* Writes the bml equivalent of the bal in linep
  * into dst, in hex. */
-/* TODO */
 int line_to_inst(int i, char **linep, char **dst)
 {
-	uint16_t i;
+	int failure;
+	uint16_t instr;
 
-	int instr = 0;
+	char *arg1, *arg2, *arg3;
+
 	char *line = *linep;
 	char *token;
 
 	/* Writes OPC, S1, and S2. */
-	token = strtok(line, " \t\v\f");
+	token = strtok(line, ", \t\v\f");
 	for (i = 0; i < NUM_INSTRUCTIONS; i++) {
 
 		if (!strcmp(line, instructions[i])) {
+
+			/* Once we are sure we have an instruction,
+			 * rip the arguments from it and get bools
+			 * of their validity. Finally, check
+			 * for trailing characters. */
+			arg1 = strtok(NULL, ", \t\v\f");
+			arg2 = strtok(NULL, ", \t\v\f");
+			arg3 = strtok(NULL, ", \t\v\f");
+
+			if (strtok(NULL, ", \t\v\f")) {
+				printf("Trailing characters on line %d.\n",
+						i);
+				return -2;
+			}
 			
 			switch(i) {
 
+				/* R-Types. */
+				case 0 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							0, 0);
+					break;
+
+				case 1 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							0, 1);
+					break;
+
+				case 2 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							1, 0);
+					break;
+
+				case 3 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							1, 1);
+					break;
+
+				case 4 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							2, 0);
+					break;
+
+				case 5 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							3, 0);
+					break;
+
+				case 6 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							3, 1);
+					break;
+
+				case 7 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							4, 0);
+					break;
+
+				case 8 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							4, 1);
+					break;
+
+				case 9 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							5, 0);
+					break;
+
+				case 10 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							5, 1);
+					break;
+
+				case 11 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							6, 0);
+					break;
+
+				case 12 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							6, 1);
+					break;
+
+				case 13 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							7, 0);
+					break;
+
+				case 14 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							7, 1);
+					break;
+
+				case 15 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							7, 2);
+					break;
+
+				case 16 :
+					failure = write_r_type(i, &instr,
+							arg1, arg2, arg3,
+							7, 3);
+					break;
+
+				/* I-Types. */
+				case 17 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							0);
+					break;
+
+				case 18 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							1);
+					break;
+
+				case 19 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							2);
+					break;
+
+				case 20 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							3);
+					break;
+
+				case 21 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							4);
+					break;
+
+				case 22 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							5);
+					break;
+
+				case 23 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							6);
+					break;
+
+				case 24 :
+					failure = write_i_type(i, &instr,
+							arg1, arg2, arg3,
+							7);
+					break;
+
+				/* S-Types. */
+				case 25 :
+					failure = write_s_type(i, &instr,
+							arg1, arg2, arg3);
+					break;
+
+				/* U-Types. */
+				case 26 :
+					failure = write_u_type(i, &instr,
+							arg1, arg2);
+					break;
+
+			}
+
+			if (failure) {
+				printf("Invalid instruction: %s at line %d.\n", line, i);
+				return -2;
 			}
 
 			break;
@@ -256,11 +488,393 @@ int line_to_inst(int i, char **linep, char **dst)
 	
 	if (i == NUM_INSTRUCTIONS) {
 		printf("Invalid instruction: %s at line %d.\n", line, i);
+		return -2;
+	}
+
+	
+	failure = inst_to_hex(instr, dst);
+	if (failure) {
+		printf("Failed to turn an instruction into hex at line %d.\n",
+				__LINE__);
+	}
+
+	return failure;
+
+}
+
+
+/* Writes inst_lst to dst in a format that logisim gets. */
+int write_instuctions(**inst_lst, const char *dst)
+{
+	int failure = 0;
+	int i = 1;
+
+	FILE *target;
+	target = fopen(dst, "w");
+	if (!target) {
+		printf("Failed to create a bml file with name %s at %d.\n",
+				dst, __LINE__);
 		return 1;
 	}
 
+	fputs("v2.0 raw\n" target);
+
+	while (*inst_lst) {
+		fputs(*inst_lst, dst);
+
+		if (i % 8 == 0)
+			fputc('\n', dst);
+		else
+			fputc(' ', dst);
+		inst_lst++;
+		i++;
+	}
+
+	fclose(target);
+	return failure;
+}
+
+
+/*
+ * ###############
+ * ### HELPERS ###
+ * ###############
+*/
+
+
+/* Reads the contents of src and puts it into buf.
+ * buf is double-null terminated. */
+int read_file(const char *src, char **buf)
+{
+	int failure = 0;
+
+	FILE *source;
+	long len;
+
+	source = fopen(dst, "rb");
+	if (!source) {
+		printf("Failed to read from file %s at %d.\n",
+				src, __LINE__);
+		return 1;
+	}
+
+	fseek(source, 0, SEEK_END);
+	len = ftell(source);
+	fseek(source, 0, SEEK_SET);
+
+	*buf = malloc((len + 2) * sizeof(char));
+	if (!*buf) {
+		printf("Failed to malloc filestring for %s at %d.\n",
+				src, __LINE__);
+		failure = 1;
+		goto close_file;
+	}
+
+	fread(*buf, 1, len, fp);
+	(*buf)[len] = '\0';
+	(*buf)[len + 1] = '\0';
+
+close_file:
+	fclose(source);
+	return failure;
+
+
+/* Frees a null-terminated list of strings. */
+int free_str_lst(char **str_lst)
+{
+	char **temp = str_lst;
+	while (*temp)
+		free(*(temp++));
+	free(str_lst);
+
+	return 0;
+}
+
+
+/* Formats instr as an R-style instruction
+ * with given arg1, arg2, and arg3, and
+ * S1 and S2. */
+int write_r_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3,
+		int s1, int s2)
+{
+
+	set_opc(0, instr);
+	set_s1(s1, instr);
+	set_s2(s2, instr);
+
+	if (is_invalid_reg(arg1)) {
+		printf("Invalid register: %s at %d.\n",
+				arg1, line);
+		return -2;
+	} else
+		decode_rsx(0, arg1, instr);
+
+	if (is_invalid_reg(arg2)) {
+		printf("Invalid register: %s at %d.\n",
+				arg2, line);
+		return -2;
+	} else
+		decode_rsx(1, arg2, instr);
+
+	if (is_invalid_reg(arg3)) {
+		printf("Invalid register: %s at %d.\n",
+				arg3, line);
+		return -2;
+	} else
+		decode_rsx(-1, arg3, instr);
+
+	return 0;
+}
+
+
+/* Formats instr as an I-style instruction
+ * with given arg1, arg2, and arg3, and S1. */
+int write_i_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3,
+		int s1)
+{
+
+	set_opc(1, instr);
+	set_s1(s1, instr);
+
+	if (is_invalid_reg(arg1)) {
+		printf("Invalid register: %s at %d.\n",
+				arg1, line);
+		return -2;
+	} else
+		decode_rsx(0, arg1, instr);
+
+	if (is_invalid_reg(arg2)) {
+		printf("Invalid register: %s at %d.\n",
+				arg2, line);
+		return -2;
+	} else
+		decode_rsx(1, arg2, instr);
+
+	if (is_invalid_imm(arg3)) {
+		printf("Invalid immediate: %s at %d.\n",
+				arg3, line);
+		return -2;
+	} else
+		decode_i_type_imm(arg3, instr);
+
+	return 0;
+}
+
+
+
+/* Formats instr as an S-style instruction
+ * with given arg1, arg2, and arg3. */
+int write_s_type(int line, uint16_t *instr,
+		char *arg1, char *arg2, char *arg3)
+{
+
+	set_opc(2, instr);
+
+	if (is_invalid_reg(arg1)) {
+		printf("Invalid register: %s at %d.\n",
+				arg1, line);
+		return -2;
+	} else
+		decode_rsx(-1, arg1, instr);
+
+	if (is_invalid_reg(arg2)) {
+		printf("Invalid register: %s at %d.\n",
+				arg2, line);
+		return -2;
+	} else
+		decode_rsx(1, arg2, instr);
+
+	if (is_invalid_imm(arg3)) {
+		printf("Invalid immediate: %s at %d.\n",
+				arg3, line);
+		return -2;
+	} else
+		decode_s_type_imm(arg3, instr);
+
+	return 0;
+}
+
+/* Formats instr as an U-style instruction
+ * with given arg1 and arg2. */
+int write_u_type(int line, uint16_t *instr,
+		char *arg1, char *arg2)
+{
+
+	set_opc(3, instr);
+
+	if (is_invalid_reg(arg1)) {
+		printf("Invalid register: %s at %d.\n",
+				arg1, line);
+		return -2;
+	} else
+		decode_rsx(0, arg1, instr);
+
+	if (is_invalid_imm(arg2)) {
+		printf("Invalid immediate: %s at %d.\n",
+				arg2, line);
+		return -2;
+	} else
+		decode_u_type_imm(arg2, instr);
+
+	return 0;
+}
+
+/* Sets the OPC field of instr to the given value. */
+int set_opc(int opc, uint16_t *instr)
+{
+	set_bit(instr, 0, get_bit(opc, 0));
+	set_bit(instr, 1, get_bit(opc, 1));
+	return 0;
+}
+
+
+/* Sets the S1 field of instr to the given value. */
+int set_s1(int s1, uint16_t *instr)
+{
+	set_bit(instr, 5, get_bit(s1, 0));
+	set_bit(instr, 6, get_bit(s1, 1));
+	set_bit(instr, 7, get_bit(s1, 2));
+	return 0;
+}
+
+
+/* Sets the S2 field of instr to the given value. */
+int set_s2(int s2, uint16_t *instr)
+{
+	set_bit(instr, 14, get_bit(s2, 0));
+	set_bit(instr, 15, get_bit(s2, 1));
+	return 0;
+}
+
+
+/* Makes sure that reg is a valid register. */
+int is_invalid_reg(char *reg)
+{
+	int i;
+
+	for (i = 0; i < NUM_REGISTERS; i++) {
+		if (!strcmp(line, registers[i]))
+			return 0;
+	}
+
+	return -2;
+}
+
+
+/* Given a valid register arg,
+ * writes it to the rsX field of instr.
+ * x < 0 ==> rs2
+ * x = 0 ==> rs0
+ * x > 0 ==> rs1	*/
+int decode_rsx(int x, char *arg, uint16_t *instr)
+{
+	int i;
+
+	for (i = 0; i < NUM_REGISTERS; i++) {
+		if (!strcmp(line, registers[i]))
+			break;
+	}
+
+
+	if (x < 0) {
+		set_bit(instr, 11, get_bit(i, 0));
+		set_bit(instr, 12, get_bit(i, 1));
+		set_bit(instr, 13, get_bit(i, 2));
+	} else if (x > 0) {
+		set_bit(instr, 8, get_bit(i, 0));
+		set_bit(instr, 9, get_bit(i, 1));
+		set_bit(instr, 10, get_bit(i, 2));
+	} else {
+		set_bit(instr, 2, get_bit(i, 0));
+		set_bit(instr, 3, get_bit(i, 1));
+		set_bit(instr, 4, get_bit(i, 2));
+	}
+
+	return 0;
 
 }
+
+
+/* Makes sure that imm is a valid decimal number. */
+int is_invalid_imm(char *imm)
+{
+	int i;
+	for (i = 0; i < strlen(imm); i++) {
+		if (imm[i] < '0' || imm[i] > '9')
+			return 1;
+	}
+	return 0;
+}
+
+
+/* Parses imm as a signed value, and writes
+ * it to I-type instr appropriately. */
+int decode_i_type_imm(char *imm, uint16_t *instr)
+{
+	long imm_l = strtol(imm, NULL, 10);
+	set_bit(instr, 11, get_bit(imm_l, 0));
+	set_bit(instr, 12, get_bit(imm_l, 1));
+	set_bit(instr, 13, get_bit(imm_l, 2));
+	set_bit(instr, 14, get_bit(imm_l, 3));
+	set_bit(instr, 15, get_bit(imm_l, 4));
+	return 0;
+
+}
+
+
+/* Parses imm as a signed value, and writes
+ * it to S-Type instr appropriately. */
+int decode_s_type_imm(char *imm, uint16_t *instr)
+{
+	long imm_l = strtol(imm, NULL, 10);
+	set_bit(instr, 5, get_bit(imm_l, 0));
+	set_bit(instr, 6, get_bit(imm_l, 1));
+	set_bit(instr, 7, get_bit(imm_l, 2));
+	set_bit(instr, 14, get_bit(imm_l, 3));
+	set_bit(instr, 15, get_bit(imm_l, 4));
+	return 0;
+}
+
+
+/* Parses imm as a signed value, and writes
+ * it to U-Type instr appropriately. */
+int decode_u_type_imm(char *imm, uint16_t *instr)
+{
+	long imm_l = strtol(imm, NULL, 10);
+	set_bit(instr, 5, get_bit(imm_l, 0));
+	set_bit(instr, 6, get_bit(imm_l, 1));
+	set_bit(instr, 7, get_bit(imm_l, 2));
+	set_bit(instr, 8, get_bit(imm_l, 3));
+	set_bit(instr, 9, get_bit(imm_l, 4));
+	set_bit(instr, 10, get_bit(imm_l, 5));
+	set_bit(instr, 11, get_bit(imm_l, 6));
+	set_bit(instr, 12, get_bit(imm_l, 7));
+	set_bit(instr, 13, get_bit(imm_l, 8));
+	set_bit(instr, 14, get_bit(imm_l, 9));
+	set_bit(instr, 15, get_bit(imm_l, 10));
+	return 0;
+}
+
+
+/* Takes instr and converts it into a 4-digit
+ * hex representation, stored in buf. */
+int instr_to_hex(uint16_t instr, char **buf);
+{
+	*buf = malloc(5 * sizeof(char));
+	if (!*buf) {
+		printf("Failed to malloc space for an instruction at %d.\n",
+				__LINE__);
+		return 1;
+	}
+
+	sprintf(*buf, "%.4x", instr);
+
+	return 0;
+
+}
+
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
---
+++ assembler.c.v
@@ -0,0 +1,156 @@
+let s:so_save = &so | let s:siso_save = &siso | set so=0 siso=0
+argglobal
+edit ~/personal/cpu/bad-machine/assembler.c
+setlocal keymap=
+setlocal noarabic
+setlocal autoindent
+setlocal balloonexpr=
+setlocal nobinary
+setlocal bufhidden=
+setlocal buflisted
+setlocal buftype=
+setlocal cindent
+setlocal cinkeys=0{,0},0),:,0#,!^F,o,O,e
+setlocal cinoptions=
+setlocal cinwords=if,else,while,do,for,switch
+setlocal colorcolumn=
+setlocal comments=sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,://
+setlocal commentstring=/*%s*/
+setlocal complete=.,w,b,u,t,i
+setlocal concealcursor=
+setlocal conceallevel=0
+setlocal completefunc=
+setlocal nocopyindent
+setlocal cryptmethod=
+setlocal nocursorbind
+setlocal nocursorcolumn
+setlocal nocursorline
+setlocal define=
+setlocal dictionary=
+setlocal nodiff
+setlocal equalprg=
+setlocal errorformat=
+setlocal noexpandtab
+if &filetype != 'c'
+setlocal filetype=c
+endif
+setlocal foldcolumn=0
+setlocal foldenable
+setlocal foldexpr=0
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldmarker={{{,}}}
+setlocal foldmethod=manual
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldtext=foldtext()
+setlocal formatexpr=
+setlocal formatoptions=croql
+setlocal formatlistpat=^\\s*\\d\\+[\\]:.)}\\t\ ]\\s*
+setlocal grepprg=
+setlocal iminsert=2
+setlocal imsearch=2
+setlocal include=
+setlocal includeexpr=
+setlocal indentexpr=
+setlocal indentkeys=0{,0},:,0#,!^F,o,O,e
+setlocal noinfercase
+setlocal iskeyword=@,48-57,_,192-255
+setlocal keywordprg=
+setlocal nolinebreak
+setlocal nolisp
+setlocal nolist
+setlocal makeprg=
+setlocal matchpairs=(:),{:},[:]
+setlocal modeline
+setlocal modifiable
+setlocal nrformats=octal,hex
+setlocal number
+setlocal numberwidth=4
+setlocal omnifunc=ccomplete#Complete
+setlocal path=
+setlocal nopreserveindent
+setlocal nopreviewwindow
+setlocal quoteescape=\\
+setlocal noreadonly
+setlocal relativenumber
+setlocal norightleft
+setlocal rightleftcmd=search
+setlocal noscrollbind
+setlocal shiftwidth=8
+setlocal noshortname
+setlocal nosmartindent
+setlocal softtabstop=0
+setlocal nospell
+setlocal spellcapcheck=[.?!]\\_[\\])'\"\	\ ]\\+
+setlocal spellfile=
+setlocal spelllang=en
+setlocal statusline=
+setlocal suffixesadd=
+setlocal swapfile
+setlocal synmaxcol=3000
+if &syntax != 'c'
+setlocal syntax=c
+endif
+setlocal tabstop=8
+setlocal tags=
+setlocal textwidth=0
+setlocal thesaurus=
+setlocal noundofile
+setlocal nowinfixheight
+setlocal nowinfixwidth
+setlocal wrap
+setlocal wrapmargin=0
+silent! normal! zE
+7,37fold
+41,127fold
+39,127fold
+129,161fold
+174,207fold
+215,275fold
+313,414fold
+416,463fold
+465,469fold
+471,475fold
+283,502fold
+509,534fold
+549,590fold
+601,627fold
+637,662fold
+672,696fold
+704,721fold
+727,729fold
+736,739fold
+746,748fold
+755,762fold
+773,796fold
+803,808fold
+816,823fold
+831,837fold
+845,857fold
+865,875fold
+883,887fold
+544,894fold
+39
+normal! zo
+41
+normal! zo
+39
+normal! zc
+283
+normal! zo
+283
+normal! zc
+544
+normal! zo
+544
+normal! zc
+let s:l = 1 - ((0 * winheight(0) + 41) / 82)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+1
+normal! 0
+let &so = s:so_save | let &siso = s:siso_save
+doautoall SessionLoadPost
+" vim: set ft=vim :
